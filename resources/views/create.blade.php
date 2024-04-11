7be82074df869a3c416e54f61ad310f7980108ed
DanyloSus <danylo.sushko19@gmail.com>
2024-04-11 20:32:54 +0300

    Validating

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -8,11 +8,17 @@
             @csrf
             <div>
                 <label for='title'>Title</label>
-                <input id='title' name='title' />
+                <input id='title' name='title' value='{{ old('title') }}' />
+                @error('title')
+                    <p>{{ $message }}</p>
+                @enderror
             </div>
             <div>
                 <label for='description'>Description</label>
-                <textarea id='description' name='description'></textarea>
+                <textarea id='description' name='description'>{{ old('description') }}</textarea>
+                @error('description')
+                    <p>{{ $message }}</p>
+                @enderror
             </div>
             <div>
                 <button type='submit'>Submit</button>
